a293c5486031218a054b68200d5faa54aabaa6aa
Mikkel Kroman <mk@maero.dk>
2010-09-20 16:30:10 +0200

    Fixed missing end keyword

--- library/dioxide/bencode.rb
+++ library/dioxide/bencode.rb
@@ -2,7 +2,7 @@
 
 class Array;      def bencode; [?l, map(&:bencode), ?e].join end end
 class String;     def bencode; [length, ?:, self].join end end
-class Fixnum;     def bencode; [?i, self, ?e].join end
+class Fixnum;     def bencode; [?i, self, ?e].join end end
 class Object;     def bencode; to_s.bencode end end
 class TrueClass;  def bencode; 1.bencode end end
 class FalseClass; def bencode; 0.bencode end end
