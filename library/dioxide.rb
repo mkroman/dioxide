b6f7ce15b5f03d3051d9d750aa28092760e32792
Mikkel Kroman <mk@maero.dk>
2010-09-20 15:33:07 +0200

    Implemented bencoding

--- library/dioxide.rb
+++ library/dioxide.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'dioxide/bencode'
+
 module Dioxide
   VERSION = [0, 0, 1]
 end
---
+++ library/dioxide/bencode.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+class Array;      def bencode; [?l, map(&:bencode), ?e].join end end
+class String;     def bencode; [length, ?:, self].join end end
+class Fixnum;     def bencode; [?i, self, ?e].join end
+class Object;     def bencode; to_s.bencode end end
+class TrueClass;  def bencode; 1.bencode end end
+class FalseClass; def bencode; 0.bencode end end
+
+class Hash
+  def bencode
+    map do |key, value|
+      [key.to_s.gsub(?_, ' '), value].map &:bencode
+    end.unshift(?d).push(?e).join
+  end
+end
