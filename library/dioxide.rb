73927b8366f5a1a679df9c823546e7d7acf9b085
Mikkel Kroman <mk@maero.dk>
2010-09-20 16:49:38 +0200

    Removed unnecessary rack/builder dependency

--- library/dioxide.rb
+++ library/dioxide.rb
@@ -1,7 +1,10 @@
 # encoding: utf-8
 
+require 'rack'
 require 'dioxide/server'
 require 'dioxide/bencode'
+require 'dioxide/request'
+require 'dioxide/response'
 
 module Dioxide
   VERSION = [0, 0, 1]
